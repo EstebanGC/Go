56bcb90bec688bf62377a87b60852f9ab1bcfd46
EstebanGC <johan.gal.car@gmail.com>
2024-06-29 18:59:31 -0500

    First steps.

---
+++ first.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hey, suck my dick!")
+
+	var age = 34
+	var car = "Mercedes"
+
+	fmt.Println("Age: ",age)
+	fmt.Println("Car: ", car)
+}
